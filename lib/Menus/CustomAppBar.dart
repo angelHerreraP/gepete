327d64c7bcabd12e78310c75b719dbb557ba9199
angelherreraP <a.manuel.herrera.p@gmail.com>
2024-12-04 12:49:19 -0600

    se generan los menus

---
+++ lib/Menus/CustomAppBar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Customappbar extends StatelessWidget {
+  const Customappbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
